4c3f120f623143e4644dbce0db4d32926179b388
iamxd <iamxd@wisebits.com>
2025-07-05 19:34:08 +0300

    Naive put data to redis

---
+++ producer/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	cryptoRand "crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	mathRand "math/rand"
+	"time"
+)
+
+func randomHash(n int) (string, error) {
+	b := make([]byte, n)
+	_, err := cryptoRand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
+
+func main() {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("could not connect to Redis: %v", err))
+	}
+
+	fmt.Println("Producer started")
+
+	for {
+		hash, err := randomHash(16)
+		if err != nil {
+			fmt.Printf("error generating random hash: %v\n", err)
+			continue
+		}
+
+		key := fmt.Sprintf("myhash:%d", time.Now().UnixNano())
+
+		err = rdb.Set(ctx, key, hash, 0).Err()
+		if err != nil {
+			fmt.Printf("error writing to redis: %v\n", err)
+			continue
+		}
+
+		fmt.Printf("Wrote to Redis: key=%s value=%s\n", key, hash)
+
+		delay := time.Duration(mathRand.Intn(1900)+100) * time.Millisecond
+		time.Sleep(delay)
+	}
+}
